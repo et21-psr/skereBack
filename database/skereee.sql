03fce55e03f76b438e767f3808cb8fcb1687f3a1
matias <mledesmaet@gmail.com>
2019-05-22 12:12:34 -0300

    create SQL

---
+++ database/skereee.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `categorias` (
+  `id_categorias` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria_padre` int(11) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_categorias`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `comentarios` (
+  `idcomentarios` int(11) NOT NULL AUTO_INCREMENT,
+  `texto` longtext,
+  `archivo` varchar(45) DEFAULT NULL,
+  `comentarioPadre` int(11) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  `posteo` int(11) DEFAULT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`idcomentarios`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `posteos` (
+  `idposteos` int(11) NOT NULL,
+  `texto` longtext,
+  `archivo` varchar(45) DEFAULT NULL,
+  `titulo` varchar(50) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  `categoria` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`idposteos`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `contrasena` varchar(45) DEFAULT NULL,
+  `nombre` char(25) DEFAULT NULL,
+  `apellido` char(25) DEFAULT NULL,
+  `foto` varchar(75) DEFAULT NULL,
+  `rol` char(25) DEFAULT NULL,
+  `clave_admin` varchar(45) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
