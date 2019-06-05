c4d69ff374a2fac27c0203a6c77f9cf1da3dd630
matias ledesma <mledesma@gmail.com>
2019-06-05 11:08:59 -0300

    SQL 5/6

--- database/skereee.sql
+++ database/skereee.sql
@@ -1,20 +1,51 @@
-create database skereee;
-use skereee;
-CREATE TABLE `categorias` (
-  `id_categorias` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
   `categoria_padre` int(11) DEFAULT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id_categorias`),
-  KEY `categoria-categoria_idx` (`categoria_padre`),
+  PRIMARY KEY (`id_categoria`),
   KEY `categorias-categorias_idx` (`categoria_padre`),
-  CONSTRAINT `categoria-categoria` FOREIGN KEY (`categoria_padre`) REFERENCES `categorias` (`id_categorias`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `categorias-categorias` FOREIGN KEY (`categoria_padre`) REFERENCES `categorias` (`id_categorias`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `categoria_padre` FOREIGN KEY (`categoria_padre`) REFERENCES `categoria` (`id_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `comentario` (
+  `id_comentario` int(11) NOT NULL AUTO_INCREMENT,
+  `texto` longtext,
+  `archivo` varchar(45) DEFAULT NULL,
+  `comentario_padre` int(11) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  `posteo` int(11) DEFAULT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_comentario`),
+  KEY `comentario_padre_idx` (`comentario_padre`),
+  KEY `usuario_idx` (`usuario`),
+  KEY `comentario_posteo_idx` (`posteo`),
+  CONSTRAINT `comentario_padre` FOREIGN KEY (`comentario_padre`) REFERENCES `comentario` (`id_comentario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `comentario_posteo` FOREIGN KEY (`posteo`) REFERENCES `posteo` (`id_posteo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `usuario_c` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `posteo` (
+  `id_posteo` int(11) NOT NULL AUTO_INCREMENT,
+  `texto` longtext,
+  `archivo` varchar(45) DEFAULT NULL,
+  `titulo` varchar(50) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  `categoria` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_posteo`),
+  KEY `posteo-categoria_idx` (`categoria`),
+  KEY `posteo-usuario_idx` (`usuario`),
+  CONSTRAINT `categoria` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `usuario` (
-  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(45) DEFAULT NULL,
   `contrasena` varchar(45) DEFAULT NULL,
   `nombre` char(25) DEFAULT NULL,
@@ -22,43 +53,8 @@ CREATE TABLE `usuario` (
   `foto` varchar(75) DEFAULT NULL,
   `rol` char(25) DEFAULT NULL,
   `clave_admin` varchar(45) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`idUsuario`),
+  PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `usuario` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `posteos` (
-  `idposteos` int(11) NOT NULL,
-  `texto` longtext,
-  `archivo` varchar(45) DEFAULT NULL,
-  `titulo` varchar(50) DEFAULT NULL,
-  `usuario` int(11) DEFAULT NULL,
-  `categoria` int(11) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`idposteos`),
-  KEY `posteo-categoria_idx` (`categoria`),
-  KEY `posteo-usuario_idx` (`usuario`),
-  CONSTRAINT `posteo-categoria` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id_categorias`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `posteo-usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `comentarios` (
-  `idcomentarios` int(11) NOT NULL AUTO_INCREMENT,
-  `texto` longtext,
-  `archivo` varchar(45) DEFAULT NULL,
-  `comentarioPadre` int(11) DEFAULT NULL,
-  `usuario` int(11) DEFAULT NULL,
-  `posteo` int(11) DEFAULT NULL,
-  `titulo` varchar(45) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`idcomentarios`),
-  KEY `comentario-comentario_idx` (`comentarioPadre`),
-  KEY `comentario-posteo_idx` (`posteo`),
-  KEY `comentario-usuario_idx` (`usuario`),
-  CONSTRAINT `comentario-comentario` FOREIGN KEY (`comentarioPadre`) REFERENCES `comentarios` (`idcomentarios`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `comentario-posteo` FOREIGN KEY (`posteo`) REFERENCES `posteos` (`idposteos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `comentario-usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
